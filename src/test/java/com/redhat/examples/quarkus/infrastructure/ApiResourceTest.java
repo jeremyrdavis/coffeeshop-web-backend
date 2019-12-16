dd4db130e7cfaf24248b28c48f359d2fd6d0f49d
Jeremy Davis <jeremyrdavis@mac.com>
2019-12-16 10:28:22 -0500

    Initial commit

---
+++ src/test/java/com/redhat/examples/quarkus/infrastructure/ApiResourceTest.java
@@ -0,0 +1,21 @@
+package com.redhat.examples.quarkus.infrastructure;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class ApiResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
